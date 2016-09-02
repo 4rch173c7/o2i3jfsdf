abdbd4353cef139155e4cabd3a813e41c7fc86be
4rch173c7 <4rch173c7@blackhat.co.nz>
2016-09-02 21:29:56 +1200

    Update nm1.py

--- prjctelur/nm1.py
+++ prjctelur/nm1.py
@@ -1,16 +1,15 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+# stdin expects multiple lines and checks their sum for each line
 import fileinput
 stdin = []
 [stdin.append(int(line)) for line in fileinput.input()]
 
 def getMulti(iteration):
-    total = 0
-        
-            for i in range(iteration):
-                        if i % 3 == 0 or i % 5 == 0:
-                                        total += i
-                                            
-                                                return total
-
-                                                for i in range(stdin[0]):
-                                                        print getMulti(stdin[i+1])
+    s1 = set(range(0,iteration,3))
+    s2 = set(range(0,iteration,5))
+    ss = s1.union(s2)
+    return sum(ss)
+    
+for i in range(stdin[0])
+    print getMulti(stdin[i+1])
+    
